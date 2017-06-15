d9cc4d9f8b9d3e54af69af09e1c8b819b79358b4
tarakan-x <vlad.chifa26@gmail.com>
2017-06-15 09:44:37 +0300

    Lab_06

---
+++ Lab_06/file transfer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace file_transfer
+{
+   
+    static class Program
+    {
+        //What we are using for ID generation
+        public static Random Rand;
+        [STAThread]
+        static void Main()
+        {
+            Rand = new Random();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
